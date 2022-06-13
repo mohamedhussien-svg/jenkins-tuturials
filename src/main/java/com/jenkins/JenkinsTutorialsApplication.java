c9ad3d672af7589d8c4f49593ba6853b3d084acd
Mohamed Hussein <mohamed.hussein@capiter.net>
2022-06-13 20:36:50 +0200

    Rename main class

--- src/main/java/com/jenkins/JenkinsTutorialsApplication.java
+++ src/main/java/com/jenkins/JenkinsTutorialsApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JenkinsTuturialsApplication {
+public class JenkinsTutorialsApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Application Started");
-		SpringApplication.run(JenkinsTuturialsApplication.class, args);
+		SpringApplication.run(JenkinsTutorialsApplication.class, args);
 	}
 
 }
